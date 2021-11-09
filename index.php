2663113d775d91a9b5562bba4721204e6d1dfbeb
CapucineWebtech <capucine.madoulaud@next-u.fr>
2021-11-09 10:20:52 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>cours 1</title>
+</head>
+<body>
+    <h1>Bienvenue. Cliquez sur un lien pour rejoindre une page.</h1>
+    <form action="" method="post">
+        <h3>Remplissez ce formulaire pour ajouter un voyage.</h3>
+        <input placeholder="Nom du pays" type="text" name="pays" required/><br>
+        <input placeholder="Date d'arrivée" type="date" name="arrivee" required/><br>
+        <input placeholder="Date de départ" type="date" name="depart" required><br>
+        <input type="submit" value="Ajouter"><br><br>
+    </form>
+</body>
+</html>
